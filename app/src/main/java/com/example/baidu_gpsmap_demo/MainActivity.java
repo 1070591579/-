b200ec1370a309aaf7d79be6a6c6f5af66e53c5b
yang <yangrui@scenauto.com>
2019-08-19 16:54:16 +0800

    第一次提交

---
+++ app/src/main/java/com/example/baidu_gpsmap_demo/MainActivity.java
@@ -0,0 +1,331 @@
+package com.example.baidu_gpsmap_demo;
+
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.Marker;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.overlayutil.WalkingRouteOverlay;
+import com.baidu.mapapi.search.core.SearchResult;
+import com.baidu.mapapi.search.route.BikingRouteResult;
+import com.baidu.mapapi.search.route.DrivingRouteResult;
+import com.baidu.mapapi.search.route.IndoorRouteResult;
+import com.baidu.mapapi.search.route.MassTransitRouteResult;
+import com.baidu.mapapi.search.route.OnGetRoutePlanResultListener;
+import com.baidu.mapapi.search.route.PlanNode;
+import com.baidu.mapapi.search.route.RoutePlanSearch;
+import com.baidu.mapapi.search.route.TransitRouteResult;
+import com.baidu.mapapi.search.route.WalkingRoutePlanOption;
+import com.baidu.mapapi.search.route.WalkingRouteResult;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";  //日志的TAG
+    private TextView locationText = null;   //显示位置信息的TextView
+    private MapView mMapView = null;   //显示地图的控件
+    private BaiduMap mBaiduMap = null; //地图管理器
+    private Marker marker = null ;     //覆盖物
+    private Context context;
+    //定位相关
+    private double mLatitude;
+    private double mLongtitude;
+    //方向传感器
+    private MyOrientationListener mMyOrientationListener;
+    private float mCurrentX;
+    //自定义图标
+    //初始化bitmap信息，不用的时候请及时回收recycle   //覆盖物图标
+    private BitmapDescriptor mIconLocation = BitmapDescriptorFactory.fromResource(R.drawable.location_arrows);
+    //定位服务的客户端。宿主程序在客户端声明此类，并调用，目前只支持在主线程中启动
+    private LocationClient mLocationClient;
+    //是否是首次定位
+    private boolean isFirstLoc  = true;
+    public BDAbstractLocationListener myListener;
+    private LatLng mLastLocationData;
+
+    // 路线规划相关
+    private RoutePlanSearch mSearch = null;
+    NetWorkStateReceiver netWorkStateReceiver;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.context = this;
+        mMapView = findViewById(R.id.b_map_View);
+        //获取地图控件引用
+        mBaiduMap = mMapView.getMap();
+        initMyLocation();
+        initPoutePlan();
+        button();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //开启定位
+        mBaiduMap.setMyLocationEnabled(true);
+        if (!mLocationClient.isStarted())
+            mLocationClient.start();
+        //开启方向传感器
+        mMyOrientationListener.start();
+    }
+
+    @Override
+    protected void onResume() {
+        if (netWorkStateReceiver == null) {
+            netWorkStateReceiver = new NetWorkStateReceiver();
+        }
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(netWorkStateReceiver, filter);
+        System.out.println("注册");
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        unregisterReceiver(netWorkStateReceiver);
+        System.out.println("注销");
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //停止定位
+        mBaiduMap.setMyLocationEnabled(false);
+        mLocationClient.stop();
+        //停止方向传感器
+        mMyOrientationListener.stop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mBaiduMap.setMyLocationEnabled(false);
+        mMapView.onDestroy();
+        mMapView = null;
+        mSearch.destroy();
+    }
+
+    @Override
+    public void onClick(View v) {
+        SDKInitializer.initialize(getApplicationContext());
+        switch (v.getId()) {
+            case R.id.but_Loc: {
+                centerToMyLocation(mLatitude, mLongtitude);
+                break;
+            }
+            case R.id.but_RoutrPlan: {
+                StarRoute();
+                break;
+            }
+        }
+    }
+
+    //按钮响应
+    private void button() {
+        //按钮
+        Button mbut_Loc = findViewById(R.id.but_Loc);
+        Button mbut_RoutrPlan = findViewById(R.id.but_RoutrPlan);
+        //按钮处理
+        mbut_Loc.setOnClickListener(this);
+        mbut_RoutrPlan.setOnClickListener(this);
+    }
+
+    //定位
+    private class MyLocationListener extends BDAbstractLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            Log.d(TAG, "BDLocationListener -> onReceiveLocation");
+            String addr; //定位结果
+            //mapView 销毁后不在处理新接收的位置
+            if (location == null || mMapView == null) {
+                Log.d(TAG, "BDLocation or mapView is null");
+                locationText.setText("定位失败...");
+                return;
+            }
+            if(!location.getLocationDescribe().isEmpty()) {
+                addr = location.getLocationDescribe();
+            }else if (location.hasAddr()) {
+                addr = location.getAddrStr();
+            }else {
+                Log.d(TAG, "BDLocation has no addr info");
+                addr = "定位失败...";
+                return;
+            }
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(mCurrentX).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locData);
+            //设置自定义图标
+            MyLocationConfiguration config = new
+                    MyLocationConfiguration(
+                    MyLocationConfiguration.LocationMode.NORMAL, true, mIconLocation);
+            mBaiduMap.setMyLocationConfiguration(config);
+            //更新经纬度
+            mLatitude = location.getLatitude();
+            mLongtitude = location.getLongitude();
+            //设置起点
+            mLastLocationData = new LatLng(mLatitude, mLongtitude);
+            if (isFirstLoc ) {
+                centerToMyLocation(location.getLatitude(), location.getLongitude());
+
+                if (location.getLocType() == BDLocation.TypeGpsLocation) {
+                    // GPS定位结果
+                    Toast.makeText(context, "定位:" + location.getAddrStr(), Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {
+                    // 网络定位结果
+                    Toast.makeText(context, "定位:" + location.getAddrStr(), Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {
+                    // 离线定位结果
+                    Toast.makeText(context, "定位:" + location.getAddrStr(), Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeServerError) {
+                    Toast.makeText(context, "定位:服务器错误", Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                    Toast.makeText(context, "定位:网络错误", Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                    Toast.makeText(context, "定位:手机模式错误，请检查是否飞行", Toast.LENGTH_SHORT).show();
+                }
+                isFirstLoc  = false;
+            }
+        }
+    }
+
+    //初始化定位
+    private void initMyLocation() {
+        //缩放地图
+        MapStatusUpdate msu = MapStatusUpdateFactory.zoomTo(15.0f);
+        mBaiduMap.setMapStatus(msu);
+        //开启定位
+        mBaiduMap.setMyLocationEnabled(true);
+        //声明LocationClient类
+        mLocationClient = new LocationClient(this);
+        //通过LocationClientOption设置LocationClient相关参数
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true); // 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setIsNeedAddress(true);//设置是否需要地址信息
+        option.setScanSpan(1000);
+        //设置locationClientOption
+        mLocationClient.setLocOption(option);
+        myListener = new MyLocationListener();
+        //注册监听函数
+        mLocationClient.registerLocationListener(myListener);
+        //初始化图标
+        mIconLocation = BitmapDescriptorFactory.fromResource(R.drawable.navi_map_gps);
+        initOrientation();
+        //开始定位
+        mLocationClient.start();
+    }
+
+    //回到定位中心
+    private void centerToMyLocation(double latitude, double longtitude) {
+        mBaiduMap.clear();
+        mLastLocationData = new LatLng(latitude, longtitude);
+        MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(mLastLocationData);
+        mBaiduMap.animateMapStatus(msu);
+    }
+
+    //传感器
+    private void initOrientation() {
+        //传感器
+        mMyOrientationListener = new MyOrientationListener(context);
+        mMyOrientationListener.setOnOrientationListener(new MyOrientationListener.OnOrientationListener() {
+            @Override
+            public void onOrientationChanged(float x) {
+                mCurrentX = x;
+            }
+        });
+    }
+
+    //路线规划初始化
+    private void initPoutePlan() {
+        mSearch = RoutePlanSearch.newInstance();
+        mSearch.setOnGetRoutePlanResultListener(listener);
+    }
+
+    // 路线规划模块
+    public OnGetRoutePlanResultListener listener = new OnGetRoutePlanResultListener() {
+        @Override
+        public void onGetWalkingRouteResult(WalkingRouteResult result) {
+            if (result == null || result.error != SearchResult.ERRORNO.NO_ERROR) {
+                Toast.makeText(MainActivity.this, "路线规划:未找到结果,检查输入", Toast.LENGTH_SHORT).show();
+                //禁止定位
+                isFirstLoc  = false;
+            }
+            assert result != null;
+            if (result.error == SearchResult.ERRORNO.AMBIGUOUS_ROURE_ADDR) {
+                // 起终点或途经点地址有岐义，通过以下接口获取建议查询信息
+                result.getSuggestAddrInfo();
+                return;
+            }
+            if (result.error == SearchResult.ERRORNO.NO_ERROR) {
+                mBaiduMap.clear();
+                Toast.makeText(MainActivity.this, "路线规划:搜索完成", Toast.LENGTH_SHORT).show();
+                WalkingRouteOverlay overlay = new WalkingRouteOverlay(mBaiduMap);
+                overlay.setData(result.getRouteLines().get(0));
+                overlay.addToMap();
+                overlay.zoomToSpan();
+            }
+            //禁止定位
+            isFirstLoc  = false;
+        }
+
+        @Override
+        public void onGetTransitRouteResult(TransitRouteResult var1) {
+        }
+
+        @Override
+        public void onGetMassTransitRouteResult(MassTransitRouteResult var1) {
+        }
+
+        @Override
+        public void onGetDrivingRouteResult(DrivingRouteResult result) {
+        }
+
+        @Override
+        public void onGetIndoorRouteResult(IndoorRouteResult var1) {
+        }
+
+        @Override
+        public void onGetBikingRouteResult(BikingRouteResult var1) {
+        }
+    };
+
+    //开始规划
+    private void StarRoute() {
+        SDKInitializer.initialize(getApplicationContext());
+        // 设置起、终点信息
+        PlanNode stNode = PlanNode.withCityNameAndPlaceName("北京", "西二旗地铁站");
+        PlanNode enNode = PlanNode.withCityNameAndPlaceName("北京", "百度科技园");
+        mSearch.walkingSearch((new WalkingRoutePlanOption())
+                .from(stNode)
+                .to(enNode));
+    }
+}
+
